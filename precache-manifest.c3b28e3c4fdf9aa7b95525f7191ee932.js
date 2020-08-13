662fa2562de95519089e10aa4444bf7c6b4e763b
Akshay <akshaysinghal181@gmail.com>
2020-08-13 21:26:06 +0530

    Updates

---
+++ precache-manifest.c3b28e3c4fdf9aa7b95525f7191ee932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ea450076f78276cd87dc1efa1f6209",
+    "url": "/aks181/Image-Gallery.git/index.html"
+  },
+  {
+    "revision": "3b0be2ccb76e45683d24",
+    "url": "/aks181/Image-Gallery.git/static/css/main.f298dd06.chunk.css"
+  },
+  {
+    "revision": "1f4bdf5a64b7d1d810b3",
+    "url": "/aks181/Image-Gallery.git/static/js/2.ca81e717.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aks181/Image-Gallery.git/static/js/2.ca81e717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0be2ccb76e45683d24",
+    "url": "/aks181/Image-Gallery.git/static/js/main.4d1a634d.chunk.js"
+  },
+  {
+    "revision": "e5969109792232f42ddd",
+    "url": "/aks181/Image-Gallery.git/static/js/runtime-main.1122340f.js"
+  }
+]);
